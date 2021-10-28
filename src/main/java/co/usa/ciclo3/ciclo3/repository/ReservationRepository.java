3b57b497370b3b9ed548edeacf295440767416e1
Juan Molano <juan.molano2.mt@correo.usa.edu.co>
2021-10-27 21:57:33 -0500

    Ciclo 3 - Retos 3, 4 y 5 Full

--- src/main/java/co/usa/ciclo3/ciclo3/repository/ReservationRepository.java
+++ src/main/java/co/usa/ciclo3/ciclo3/repository/ReservationRepository.java
@@ -1,8 +1,13 @@
 package co.usa.ciclo3.ciclo3.repository;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import co.usa.ciclo3.ciclo3.model.Client;
 import co.usa.ciclo3.ciclo3.model.Reservation;
+import co.usa.ciclo3.ciclo3.reports.ClientCount;
 import co.usa.ciclo3.ciclo3.repository.crud.ReservationCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -17,4 +22,16 @@ public class ReservationRepository {
     public Optional<Reservation> getReservation(int id){return CrudRepository.findById(id);}
     public Reservation save(Reservation reservation){return CrudRepository.save(reservation);}
     public void delete(Reservation reservation){CrudRepository.delete(reservation);}
+
+    public List<Reservation> ReservationStatus (String status){return CrudRepository.findAllByStatus(status);}
+    public List<Reservation> ReservationTime(Date a, Date b){return CrudRepository.findAllByStartDateAfterAndStartDateBefore(a,b);}
+
+    public List<ClientCount> getTopClient(){
+        List<ClientCount> res = new ArrayList<>();
+        List<Object[]>report = CrudRepository.countTotalReservationsByClient();
+        for (int i=0; i<report.size();i++){
+            res.add(new ClientCount((long)report.get(i)[1],(Client)report.get(i)[0]));
+        }
+        return res;
+    }
 }
--- src/main/java/co/usa/ciclo3/ciclo3/repository/crud/ReservationCrudRepository.java
+++ src/main/java/co/usa/ciclo3/ciclo3/repository/crud/ReservationCrudRepository.java
@@ -1,7 +1,15 @@
 package co.usa.ciclo3.ciclo3.repository.crud;
 
+import java.util.Date;
+import java.util.List;
 import co.usa.ciclo3.ciclo3.model.Reservation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReservationCrudRepository extends CrudRepository <Reservation,Integer> {
+    public List<Reservation> findAllByStatus (String status);
+    public List<Reservation> findAllByStartDateAfterAndStartDateBefore (Date dateOne, Date dateTwo);
+
+    @Query("SELECT c.client, COUNT(c.client) FROM Reservation AS c GROUP BY c.client ORDER BY COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationsByClient();
 }
